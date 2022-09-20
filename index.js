aec751df97e8f713abd12160cc8972e37b06340e
Vichiuri <chiuri.vic@gmail.com>
2022-09-20 17:04:25 +0300

    post courses by id

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express ();
 
+app.use(express.json());
+
 const courses = [ { id :1, name:'course1'},
 {id :2, name:'course2'},
 {id :3, name:'course3'}
@@ -29,12 +31,22 @@ app.get ('/api/courses/:id', (req, res) => {
 } );
 
 
+app.post ('/api/courses', (req, res) => {
+const course = {
+    id: courses.length + 1,
+    name:req.body.name
+};
+
+courses.push(course);
+res.send(course);
+
+  
+} );
+
 //PORT
 const port = process.env.PORT || 3000;
 
 app.listen(port ,() => console.log (`listening on port ${port}.... `));
-
-
 
 
 
